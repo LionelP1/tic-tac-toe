a7d97f85092604852c22f2de860690825e45fa49
Lionel <lpei1900@gmail.com>
2024-07-08 13:28:55 -0400

    Finished game controller module

--- script.js
+++ script.js
@@ -37,6 +37,34 @@ const gameController = (() => {
     let move = 1;
     let isOver = false;
 
+
+    const playMove = (inputIndex) => {
+        gameBoard.setBoardIndex(inputIndex,getCurrentPlayerSign());
+        //Now display the symbol on screen
+
+        if(checkWin(inputIndex)){
+            //Display Message
+            isOver = true;
+            return;
+        }
+
+        if(move === 9){
+            //display message:Draw
+            isOver = true;
+            return;
+        }
+        move++;
+    };
+    
+    const reset = () => {
+        move = 1;
+        isOver = false;
+    };
+
+    const getIsOver = () => {
+        return isOver;
+      };
+    
     const getCurrentPlayerSign = () => {
         if(move % 2 === 1){
             return playerO.getSign();
@@ -46,30 +74,27 @@ const gameController = (() => {
         }
     };
 
-
-    const playMove = (fieldIndex) => {
-        gameBoard.setBoardIndex(fieldIndex,getCurrentPlayerSign());
-        //Now display the symbol on screen
-
-
-        if(/*Player Wins */){
-            //Display Message
-            //return
-        }
-
-        if(round === 9){
-            //display message:Draw
-            //Return
-        }
-
-
-        move++;
-
-
-
-    };
+    const checkWin = (inputIndex) => {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6] // Diagonals
+        ];
     
+        for (let combo of winningCombinations) {
+            if (combo.includes(inputIndex)) {
+                const [a, b, c] = combo;
+                if (gameBoard.getBoardIndex(combo[0]) !== "" &&
+                gameBoard.getBoardIndex(combo[0]) === gameBoard.getBoardIndex(combo[1]) &&
+                gameBoard.getBoardIndex(combo[0]) === gameBoard.getBoardIndex(combo[2])) {
+                return true;
+                }
+            }
+        }
+        return false;
+    };
 
+    return {playMove, reset, getIsOver};
 
 })();
 
